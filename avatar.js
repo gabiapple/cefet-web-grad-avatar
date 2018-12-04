639b8aa156fd6e3862177d71e4e36ad11f913983
Gabriela Ramalho <gabrielaramalho96@gmail.com>
2018-12-04 11:18:25 -0200

    Exercicio2

--- avatar.js
+++ avatar.js
@@ -4,3 +4,16 @@ nome.addEventListener("input", () => {
 	let outputName = document.getElementById("avatar-nome");
 	outputName.innerHTML = nome.value;
 });
+
+	console.log("oi");
+
+let color = document.getElementById("input-color");
+
+color.addEventListener("change", () => {
+	let outputCabeca = document.getElementById("avatar-cabeca");
+	let outputCorpo = document.getElementById("avatar-corpo");
+	outputCorpo.style.backgroundColor = color.value; 
+	outputCabeca.style.backgroundColor = color.value;
+});
+
+// -------
